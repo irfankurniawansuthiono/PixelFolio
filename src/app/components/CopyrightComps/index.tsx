751e17392d855c2a57d55174df994ccea275e2e2
Irfan Kurniawan <kurniawanmarc93@gmail.com>
2025-01-07 19:30:31 +0700

    Refactor CopyrightComps layout for improved responsiveness and readability

--- src/app/components/CopyrightComps/index.tsx
+++ src/app/components/CopyrightComps/index.tsx
@@ -1,12 +1,7 @@
 export default function CopyrightComps() {
     return (
-        <div className="fixed bottom-6 flex justify-center items-center gap-2">
-            <div className="flex md:hidden">
+        <div className="fixed bottom-6 md:bottom-3 flex justify-center items-center gap-2">
                 <i className="nes-icon coin is-small"></i>
-            </div>
-            <div className="hidden md:flex">
-                <i className="nes-icon coin is-medium"></i>
-            </div>
             <span className="md:text-xl text-xs text-nowrap text-white">Copyright &copy;{new Date().getFullYear()} IrfanKS</span>
         </div>
     );
